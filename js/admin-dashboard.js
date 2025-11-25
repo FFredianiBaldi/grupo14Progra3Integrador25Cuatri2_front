d7cf435d70dcf8af98f4aaecaf4e69c891ab2338
fedefredianib <fedfredbal@gmail.com>
2025-11-25 12:13:36 -0300

    dashboard creado, falta conectar con el back

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -4,9 +4,51 @@ if(!token){
     window.location.href = "login.html";
 }
 
-const cerrarSesion = document.getElementById("cerrar-sesion");
+const links = document.querySelectorAll(".aside-superior a");
+const productosHTML = document.querySelector(".productos");
+const ventasHTML = document.querySelector(".ventas");
 
-cerrarSesion.addEventListener("click", () => {
-    localStorage.removeItem("token");
-    window.location.href = "login.html";
-});
+
+function cerrarSesion(){
+    const cerrarSesion = document.getElementById("cerrar-sesion");
+    
+    cerrarSesion.addEventListener("click", () => {
+        localStorage.removeItem("token");
+        window.location.href = "login.html";
+    });
+}
+
+
+function productos(){
+    document.getElementById("btn-productos").addEventListener("click", async (evento) => {
+        evento.preventDefault();
+    
+        const resp = await fetch ("./productos.html");
+        const html = await resp.text();
+        productosHTML.innerHTML = html;
+
+        ventasHTML.innerHTML = "";
+    
+        links.forEach(a => a.classList.remove("activo"));
+        evento.target.classList.add("activo");
+    });
+}
+
+function ventas(){
+    document.getElementById("btn-ventas").addEventListener("click", async (evento) => {
+        evento.preventDefault();
+
+        const resp = await fetch("./ventas.html");
+        const html = await resp.text();
+        ventasHTML.innerHTML = html;
+
+        productosHTML.innerHTML = "";
+
+        links.forEach(a => a.classList.remove("activo"));
+        evento.target.classList.add("activo");
+    });
+}
+
+cerrarSesion();
+productos();
+ventas();
